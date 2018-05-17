2e69f297e0148b543ced7a383c1bef02019d75e5
publicmint <public.mint@gmail.com>
2018-05-17 13:09:16 +0900

    ホームディレクトリを相対パスに修正

--- dein.install.sh
+++ dein.install.sh
@@ -1,4 +1,4 @@
   curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh
   chmod +x /tmp/installer.sh
-  mkdir -p /root/.vim/dein/
+  mkdir -p ~/.vim/dein/
   sh /tmp/installer.sh ~/.vim/dein
